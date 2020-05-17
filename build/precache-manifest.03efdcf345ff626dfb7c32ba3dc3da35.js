e2f7657748135f0d4edf7db8f1499add0ffeb04e
sheraj <sheraj@SHERAJ-IN.in.oracle.com>
2020-05-17 20:03:32 +0530

    initial commit

---
+++ build/precache-manifest.03efdcf345ff626dfb7c32ba3dc3da35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "567ffce0ae9bad4dfbdac99bc247bf3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c728eb14d4412ad4ff4",
+    "url": "/static/css/main.43ddd47b.chunk.css"
+  },
+  {
+    "revision": "d0a0522f7235108bfdf0",
+    "url": "/static/js/2.cd439bb4.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/static/js/2.cd439bb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c728eb14d4412ad4ff4",
+    "url": "/static/js/main.506f2a59.chunk.js"
+  },
+  {
+    "revision": "1d52a7af4758fb652f20",
+    "url": "/static/js/runtime-main.04de2117.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
